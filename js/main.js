8edb57213d160b7d44f393a2cc6ed0804ef3f69c
Nicole Sousa <nicolesousa@alu.ufc.br>
2022-11-20 22:08:24 -0300

    new file:   assets/images/mapa_geral_2.png 	new file:   assets/images/mapa_geral_3.png 	new file:   assets/images/regiao_1.png 	new file:   assets/images/regiao_2.png 	new file:   assets/images/regiao_3.png 	new file:   assets/images/regiao_4.png 	new file:   assets/svg/regiao_1.svg 	new file:   assets/svg/regiao_2.svg 	new file:   assets/svg/regiao_3.svg 	modified:   css/main.css 	new file:   css/svg.css 	modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
+// Declarações Globais
 const viewport = document.querySelector('.app');
 const content = viewport.querySelector('.app-inner');
 
+// ScrollBooster - Arrastar tela - página inicial
 let sb = new ScrollBooster({
     viewport: viewport,
-    // content: content,
-    // textSelection: true,
     emulateScroll: false,
     bounce: false,
     shouldScroll: (data, event) => {
         return event.target.tagName !== 'BUTTON';
     },
     onUpdate: (data) => {
-        // viewport.scrollLeft = data.position.x
-        // viewport.scrollTop = data.position.y
         content.style.transform = `translate(
         ${-data.position.x}px,
         ${-data.position.y}px
@@ -21,4 +19,32 @@ let sb = new ScrollBooster({
     onClick() {
         console.log('clicked')
     }
-})
+});
+
+// Left Sidebar Navigation
+const sideNavAnimals = document.querySelector(".sidenav-animals");
+const handle = sideNavAnimals.querySelector(".arrow-toggle");
+const icon_handle = document.querySelector(".arrow-toggle i");
+
+handle.onclick = function () {
+  sideNavAnimals.classList.toggle("active");
+  icon_handle.classList.toggle("icon_rotate");
+};
+
+$(".arrow-up").click(function () {
+  $(".navigation ul").animate({
+    scrollTop: "-=65px"
+  });
+});
+
+$(".arrow-down").click(function () {
+  $(".navigation ul").animate({
+    scrollTop: "+=65px"
+  });
+});
+
+/* Hover Regions SVG */
+const inner = document.querySelector('.region_1_svg');
+
+inner.addEventListener('mouseenter', () => inner.classList.add('hovered'));
+inner.addEventListener('mouseleave', () => inner.classList.remove('hovered'));
